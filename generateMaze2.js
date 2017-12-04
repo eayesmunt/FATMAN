d18f41c2353e02ae60d323e52db8860d3e7f7121
ekraihan <ekraihan@iupui.edu>
2017-12-04 18:24:40 -0500

    added building creation

--- generateMaze2.js
+++ generateMaze2.js
@@ -1,4 +1,4 @@
-let _ = require("underscore");
+// let _ = require("underscore");
 
 let Directions = [
   {
@@ -57,26 +57,29 @@ function carvePath(curX, curY) {
   let directions = _.shuffle(Directions);
 
   directions.forEach(direction => {
+
     let newX = curX + direction.DX*2;
     let newY = curY + direction.DY*2;
 
     if (map[newX] && map[newX][newY] && map[newX][newY].directions.length == 0){
       map[curX][curY].directions.push(direction.direction);
       map[curX+direction.DX][curY+direction.DY].isBuilding = false;
-      map[curX+direction.DX][curY+direction.DY].directions.push(direction);
+      map[curX+direction.DX][curY+direction.DY].directions.push(direction.direction);
       map[curX+direction.DX][curY+direction.DY].directions.push(direction.opposite);
       map[newX][newY].directions.push(direction.opposite);
       map[newX][newY].isBuilding = false;
-      map[curX][curY].northBId = Math.round(Math.random()*3);
-      map[curX][curY].southBId = Math.round(Math.random()*3);
-      map[curX][curY].eastBId = Math.round(Math.random()*3);
-      map[curX][curY].westBId = Math.round(Math.random()*3);
+      map[curX+direction.DX][curY+direction.DY].northBId = Math.round(Math.random()*3);
+      map[curX+direction.DX][curY+direction.DY].southBId = Math.round(Math.random()*3);
+      map[curX+direction.DX][curY+direction.DY].eastBId = Math.round(Math.random()*3);
+      map[curX+direction.DX][curY+direction.DY].westBId = Math.round(Math.random()*3);
+
+      map[curX][curY].northBId = direction.direction == "N" ? 4 : Math.round(Math.random()*3);
+      map[curX][curY].southBId = direction.direction == "S" ? 4 : Math.round(Math.random()*3);
+      map[curX][curY].eastBId = direction.direction == "E" ? 4 : Math.round(Math.random()*3);
+      map[curX][curY].westBId = direction.direction == "W" ? 4 : Math.round(Math.random()*3);
       carvePath(newX, newY);
     }
   })
 }
 map[1][1].isBuilding = false;
 carvePath(1,1)
-
-// let blah = _.shuffle(Directions)
-// console.log(blah)
